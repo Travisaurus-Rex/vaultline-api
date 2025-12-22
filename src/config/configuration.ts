bdef3622fb935f4201a32bd1197784e03cb2dc45
Travis Adams <brokenCD@live.com>
2025-12-22 13:09:24 +0700

    setup initial configuration

---
+++ src/config/configuration.ts
@@ -0,0 +1,13 @@
+export const configuration = () => ({
+  env: process.env.NODE_ENV,
+  server: {
+    port: Number(process.env.PORT) || 3000,
+  },
+  database: {
+    url: process.env.DATABASE_URL,
+  },
+  auth: {
+    jwtAccessSecret: process.env.JWT_ACESS_SECRET,
+    jwtRefreshSecret: process.env.JWT_REFRESH_SECRET,
+  },
+});
---
+++ src/config/env.validation.ts
@@ -0,0 +1,12 @@
+import * as Joi from 'joi';
+
+export const envValidationSchema = Joi.object({
+  NODE_ENV: Joi.string().valid('dev', 'staging', 'prod').required(),
+
+  PORT: Joi.number().default(3000),
+
+  DATABASE_URL: Joi.string().uri().required(),
+
+  JWT_ACCESS_SECRET: Joi.string().min(32).required(),
+  JWT_REFRESH_SECRET: Joi.string().min(32).required(),
+});
