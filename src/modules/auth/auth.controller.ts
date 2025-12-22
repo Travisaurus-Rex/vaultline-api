0c4636ea0fad5a974ba265d907128e946ba05777
Travis Adams <brokenCD@live.com>
2025-12-22 15:00:05 +0700

    hook up auth controller with auth service

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,19 +1,27 @@
 import { Controller, Post, UseGuards } from '@nestjs/common';
+import { Role } from 'src/common/constants/roles';
 import { User } from 'src/common/decorators/user.decorator';
 import { RefreshTokenGuard } from 'src/common/guards/refresh-token.guard';
+import { AuthService } from './auth.service';
+
+type User = {
+  id: string;
+  roles: Role[];
+};
 
 @Controller('auth')
 export class AuthController {
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
   @Post('login')
   login() {
-    return { message: 'login placeholder' };
+    const user = { id: 'user123', roles: [Role.USER] };
+    return this.authService.issueTokens(user);
   }
 
   @Post('refresh')
   @UseGuards(RefreshTokenGuard)
-  refresh(@User() user: any) {
-    return { message: 'refresh placeholder' };
+  refresh(@User() user: User) {
+    return this.authService.issueTokens(user);
   }
 }
