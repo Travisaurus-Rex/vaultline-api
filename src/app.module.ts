1552159f61d617a4940db310fac71bd7fa83cd82
Travis Adams <brokenCD@live.com>
2025-12-22 13:40:20 +0700

    use throttler to enable rate limiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,21 @@ import { ConfigModule } from '@nestjs/config';
 import { configuration } from './config';
 import { HealthModule } from './health/health.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       load: [configuration],
+    }),
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60,
+          limit: 100,
+        },
+      ],
     }),
     HealthModule,
     AuthModule,
